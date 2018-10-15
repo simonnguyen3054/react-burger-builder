01e6cbc797f548caff79ea029f716c7960fb8829
Simon Nguyen <simonnguyen3054@gmail.com>
2018-10-15 11:28:48 -0700

    output multiple build buttons

--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
 import './BuildControls.css';
 
+const controls = [
+  {label: 'Salad', type: 'salad'},
+  {label: 'Bacon', type: 'bacon'},
+  {label: 'Cheese', type: 'cheese'},
+  {label: 'Meat', type: 'meat'}
+];
 
 const BuildControls = (props) => {
   return (
-    <div>
-
+    <div className="BuildControls">
+      {controls.map(ctrl => (
+        <BuildControl key={ctrl.label} label={ctrl.label}/>
+      ))}
     </div>
   )
 }
