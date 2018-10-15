c36e09f6adf9729343711cbbe1708f6da31cd5e1
Simon Nguyen <simonnguyen3054@gmail.com>
2018-10-15 10:44:34 -0700

    output burger ingredients dynamically

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -3,11 +3,21 @@ import './Burger.css';
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
 
 const Burger = (props) => {
+  //First we get an array of keys from the object ingredients
+  //We map over each key and return a new array. props.ingredients[igKey] gives us the number of indices in the new array
+  //We then map over the new array. We don't care about the key of this new array. We care only the index of this new array
+  //the final return gives us the BurgerIngredients elements.
+  const transformedIngredients = Object.keys(props.ingredients)
+      .map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+          return <BurgerIngredients key={igKey + i} type={igKey}/>
+        });
+      });
+
   return (
     <div className="Burger">
       <BurgerIngredients type="bread-top"/>
-      <BurgerIngredients type="cheese"/>
-      <BurgerIngredients type="meat"/>
+      {transformedIngredients}
       <BurgerIngredients type="bread-bottom"/>
     </div>
   )
--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      met: 2
+    }
+  }
+
   render () {
     return (
       <div>
-        <Burger />
+        <Burger ingredients={this.state.ingredients}/>
         <div>Build Controls</div>
       </div>
     )
